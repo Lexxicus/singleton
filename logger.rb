b567789e1c6ea89dcc7cfd24ad95a53cc5c27d5d
Lexxicus <alexxxicus@gmail.com>
2019-12-20 15:29:56 +0500

    Refactorin: use ruby system lib singleton

--- logger.rb
+++ logger.rb
@@ -1,23 +1,28 @@
+require 'singleton'
+
 class Logger
+  include Singleton
   # открываем файл
   def initialize
     @f = File.open 'log.txt', 'a'
   end
-  @@x = Logger.new
-  def self.instance
-    return @@x
-  end
+ # заменили га инклуд синглетон
+  #@@x = Logger.new
+  #def self.instance
+  #  return @@x
+  #end
   # метод класса
-  def self.say_something
-    puts "haha"
-  end
+  #def self.say_something
+  #  puts "haha"
+  #end
   # метод объекта, экземляра
   def log_something what
     puts 'Пишу в файл'
     sleep 0.2
     @f.puts what
+    @f.flush # flush записывает на диск
   end
   # метод new может вызываться только внутри самого класса,
   # защита от создания нового объекта из вне класса
-  private_class_method :new
+  # заменили на сиглетон библиотеку руби private_class_method :new
 end
